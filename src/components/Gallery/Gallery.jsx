a2cb7d3f1c983005984f812c96e2113f120264f8
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-09-16 00:16:21 +0500

    commit

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -27,7 +27,77 @@ function Gallery() {
         <img src={Gazon8} alt="" className="gallery-img" />
         <img src={Gazon9} alt="" className="gallery-img" />
         <img src={Gazon10} alt="" className="gallery-img" />
+        <img
+          src="https://avatars.mds.yandex.net/i?id=0499e45754447e04dcc197224b60646ca4386f76-8981391-images-thumbs&n=13"
+          alt=""
+          className="gallery-img"
+        />
+        <img
+          src="https://avatars.mds.yandex.net/i?id=45d1752b9638a97fe1972f92d1ad2471ffa5863f-5388696-images-thumbs&n=13"
+          alt=""
+          className="gallery-img"
+        />
+        <img
+          src="https://frankfurt.apollo.olxcdn.com/v1/files/ksdby15wj7gh-UZ/image;s=1600x1200"
+          alt=""
+          className="gallery-img"
+        />
+        <img
+          src="https://avatars.mds.yandex.net/i?id=dc3a8ca5b5539397721c1a7563aadfd6592c0d56-8244444-images-thumbs&n=13"
+          alt=""
+          className="gallery-img"
+        />
+        <img
+          src="https://avatars.mds.yandex.net/i?id=cee244732087b85fae601b9939fd63af4bbc176b-9592077-images-thumbs&n=13"
+          alt=""
+          className="gallery-img"
+        />
+        <img
+          src="https://avatars.mds.yandex.net/get-altay/2094876/2a0000016ccd19e9a6342ce8dac05d874d6f/XXL_height"
+          alt=""
+          className="gallery-img"
+        />
       </div>
+      <iframe
+        width="400"
+        height="280"
+        src="https://www.youtube.com/embed/GxJ7Nk__ZS4?si=QJAjBc1KYkjqYmou"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+      <iframe
+        width="400"
+        height="280"
+        src="https://www.youtube.com/embed/mSEGc_t9-dQ?si=PiIHoN1BatPdm66e"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+      <iframe
+        width="400"
+        height="280"
+        src="https://www.youtube.com/embed/evUIAilkfEQ?si=vTF4mgm2bUtF82lB"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+      <iframe
+        width="400"
+        height="280"
+        src="https://www.youtube.com/embed/_Vea6KgR_ss?si=2ZU8sGAmyXxnl0-e"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+      <iframe
+        width="400"
+        height="280"
+        src="https://www.youtube.com/embed/p7yX9kjIldE?si=A4gx8ClFUleWOF9_"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
       <Link to="/gazon" className="gallery-link">
         Увидеть все
       </Link>
